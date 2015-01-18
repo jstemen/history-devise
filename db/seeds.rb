2fda3a4a2af8c2197510e32cbc24da8277ea4294
Jared Stemen <jstemen@gmail.com>
2015-01-17 20:00:10 -0800

    Adding in jstemen@gmail.com user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@
 
 App.create(title: 'facebook', url: 'https://fbstatic-a.akamaihd.net/rsrc.php/v2/yJ/r/XZU5QNqW7-3.js')
 
+App.create(title: 'twitter', url: 'https://abs.twimg.com/a/1421172151/css/t1/twitter_core.bundle.css')
+App.create(title: 'slickdeals', url: 'http://css.slickdealscdn.com/min/3729/g=css&style=14&n=tutorial')
+
+
+User.create(first_name: 'Jared', last_name: 'Stemen', email: 'jstemen@gmail.com',password: 'abcdefgh', password_confirmation: 'abcdefgh')
