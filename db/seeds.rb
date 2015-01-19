0dc4bac6bbc32a054f4916f430c0e9fde22f818f
Jared Stemen <jstemen@gmail.com>
2015-01-18 16:55:22 -0800

    adding in myspace

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ App.create(title: 'facebook', url: 'https://fbstatic-a.akamaihd.net/rsrc.php/v2/
 App.create(title: 'twitter', url: 'https://abs.twimg.com/a/1421172151/css/t1/twitter_core.bundle.css')
 App.create(title: 'slickdeals', url: 'http://css.slickdealscdn.com/min/3729/g=css&style=14&n=tutorial')
 App.create(title: 'nhl', url: 'http://cdn.nhle.com/projects/ice3-ui/com.nhl.ice3.ui.t5.components/GlobalPageImports/dist/css/_GlobalPageImports.min.css?v=8.14')
+App.create(title: 'myspace', url: 'https://x.myspacecdn.com/new/common/css/sprite.F6FA0BA48063DD1BEC75ED44175CB0AB.min.css')
 
 
 User.create(first_name: 'Jared', last_name: 'Stemen', email: 'jstemen@gmail.com',password: 'abcdefgh', password_confirmation: 'abcdefgh')
